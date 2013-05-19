42c019933db6ef9b658555fb46251c9cf7ada30b
hovmand <jonas@hovmand.eu>
2013-05-19 19:51:54 +0200

    Successful validation of accept from server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"crypto/rand"
+	"crypto/sha1"
 	"encoding/base64"
 	"io"
 )
@@ -38,31 +39,51 @@ func main() {
 	}
 
 	fmt.Println(resp)
+	
+	if standard.VerifyResponse(resp) != true {
+		fmt.Println("==\n==", "Wrong accept from server", "\n==")
+	}
 }
 
 type WebsocketStandard interface {
 	AddHeaders(req *http.Request)
-	VerifyResponse(resp http.Response) bool
+	VerifyResponse(resp *http.Response) bool
 }
 
-type WebsocketStandard_13 struct {}
+type WebsocketStandard_13 struct {
+	Key string
+}
 
-func (s WebsocketStandard_13) AddHeaders(req *http.Request) {
+func (s *WebsocketStandard_13) AddHeaders(req *http.Request) {
 	req.Header.Add("Upgrade", "websocket")
 	req.Header.Add("Connection", "Upgrade")
 	req.Header.Add("Sec-WebSocket-Version", "13")
 	
-	buf := make([]byte, 16) 
-	_, err := io.ReadFull(rand.Reader, buf) 
+	buf := make([]byte, 16)
+	_, err := io.ReadFull(rand.Reader, buf)
 	
 	if err != nil {
 		panic(fmt.Errorf("ERROR", err))
 	}
 	
-	req.Header.Add("Sec-WebSocket-Key", base64.StdEncoding.EncodeToString(buf))
+	s.Key = base64.StdEncoding.EncodeToString(buf)
+	
+	req.Header.Add("Sec-WebSocket-Key", s.Key)
 }
 
-func (s WebsocketStandard_13) VerifyResponse(resp http.Response) (b bool) {
-	return true
+func (s *WebsocketStandard_13) VerifyResponse(resp *http.Response) (b bool) {
+	h := sha1.New()
+	
+	p := []byte(s.Key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+	
+	_, err := h.Write(p)
+	
+	if err != nil {
+		panic(fmt.Errorf("ERROR", err))
+	}
+	
+	expected := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	
+	return resp.Header.Get("Sec-WebSocket-Accept") == expected
 }
 
